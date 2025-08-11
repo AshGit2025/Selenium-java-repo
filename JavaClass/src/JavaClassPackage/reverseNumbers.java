66d5ba4227056d16173fbe6afe4ac47885618e26
User <User@DESKTOP-0SGD7C0.powerhub>
2025-08-11 07:21:48 +0100

    11Aug

---
+++ JavaClass/src/JavaClassPackage/reverseNumbers.java
@@ -0,0 +1,19 @@
+package JavaClassPackage;
+
+public class reverseNumbers {
+
+	public static void main(String[] args) {
+		for (int x=1; x<=4; x++)
+		{
+			System.out.print(x + " ");
+		}
+		System.out.println(" ");
+		
+		for (int i=4; i>=1; i--)
+		{
+			System.out.print(i + " ");
+		}
+
+	}
+
+}
